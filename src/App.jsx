07157c3291852f68357294a1dbfffd4e7d2cfeff
Siarhei Parkhamchuk <s.parkhamchuk@gmail.com>
2019-06-28 15:00:45 +0300

    project structure added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Nav from './components/Nav';
+import Header from './components/Header';
+import Profile from './components/Profile';
+
+const App = () => {
+  return (
+    <div className="app-wrapper">
+      <Header />
+      <Nav />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="http://logodust.com/img/free/logo46.png" alt="Logo" />
+        </header>
+    )
+}
+
+export default Header;
