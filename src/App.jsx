4209581c56b2d0d36536d0278589ed0b5ae6595b
Siarhei Parkhamchuk <s.parkhamchuk@gmail.com>
2019-07-09 01:17:37 +0300

    Dialogs added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import './App.css';
 import Nav from './components/Nav';
 import Header from './components/Header';
 import Profile from './components/Profile';
+import Dialogs from './components/Dialogs';
 
 const App = () => {
 	return (
-		<div className="app-wrapper">
+		<div className='app-wrapper'>
 			<Header />
 			<Nav />
-			<Profile />
+			<div className='app-wrapper__content'>
+				<Profile />
+				{/* <Dialogs /> */}
+			</div>
 		</div>
 	);
 }
--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts';
 
 const Profile = () => {
     return (
-        <div className={css.content}>
+        <div>
             <div className={css.header__img}>
                 <img src="http://www.b17.ru/foto/uploaded/upl_1536215588_12069.jpg" alt="" />
             </div>
